4900b3f71fc51f3c46a1c4a93d685ad4dfb1fa0a
Stephen Dolan <dolan.stephen1@gmail.com>
2020-08-11 00:18:03 -0400

    Consistent styling

--- src/pages/dashboard/index_page.cr
+++ src/pages/dashboard/index_page.cr
@@ -6,7 +6,7 @@ class Dashboard::IndexPage < MainLayout
   def content
     div class: "bg-white overflow-hidden shadow rounded-lg" do
       div class: "border-b border-gray-200 px-4 py-5 sm:px-6" do
-        h1 "Your Dashboard", class: "font-bold text-lg"
+        h1 "Your Dashboard", class: "font-medium text-lg"
       end
 
       div class: "px-4 py-5 sm:p-6" do
--- src/pages/me/show_page.cr
+++ src/pages/me/show_page.cr
@@ -6,10 +6,10 @@ class Me::ShowPage < MainLayout
   def content
     div class: "bg-white overflow-hidden shadow rounded-lg" do
       div class: "border-b border-gray-200 px-4 py-5 sm:px-6" do
-        h1 "Your profile"
+        h1 "Your profile", class: "font-medium text-lg"
       end
       div class: "px-4 py-5 sm:p-6" do
-        h3 "Email:  #{@current_user.email}"
+        h3 "Email:  #{current_user.email}"
       end
     end
   end
