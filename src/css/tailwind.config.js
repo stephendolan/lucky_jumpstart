92a52ef37668a824beecb6beae79a33c55c6cd6f
Stephen Dolan <dolan.stephen1@gmail.com>
2020-09-17 10:17:52 -0400

    Incorporate new "Future" flag to reduce PurgeCSS size

--- src/css/tailwind.config.js
+++ src/css/tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: [
     "./src/css/**/*.scss",
