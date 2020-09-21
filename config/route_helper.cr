14231de30098a0c996270522f2d73ade47ad43df
Stephen Dolan <dolan.stephen1@gmail.com>
2020-09-21 18:25:01 -0400

    Rename my_app to lucky_jumpstart

--- config/route_helper.cr
+++ config/route_helper.cr
@@ -1,7 +1,7 @@
 # This is used when generating URLs for your application
 Lucky::RouteHelper.configure do |settings|
   if Lucky::Env.production?
-    # Example: https://my_app.com
+    # Example: https://lucky_jumpstart.com
     settings.base_uri = ENV.fetch("APP_DOMAIN")
   else
     # Set domain to the default host/port in development/test
