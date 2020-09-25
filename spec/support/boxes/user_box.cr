4f3d2ccfa302b2d5b0a1e8b534b1bbf326929f83
Stephen Dolan <dolan.stephen1@gmail.com>
2020-09-25 09:13:42 -0400

    Update user box with confirmation

--- spec/support/boxes/user_box.cr
+++ spec/support/boxes/user_box.cr
@@ -2,5 +2,7 @@ class UserBox < Avram::Box
   def initialize
     email "#{sequence("test-email")}@example.com"
     encrypted_password Authentic.generate_encrypted_password("password")
+    confirmation_token "#{Random::Secure.urlsafe_base64(32)}"
+    confirmed_at Time.utc
   end
 end
