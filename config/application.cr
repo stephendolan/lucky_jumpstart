5c7c1fad9f30d58e73ccee1cc9db65ea2f1fbba4
Stephen Dolan <dolan.stephen1@gmail.com>
2021-04-24 19:22:38 -0400

    Update default support_email setting to use a Carbon::Address object.

--- config/application.cr
+++ config/application.cr
@@ -1,11 +1,11 @@
 class Application
   Habitat.create do
     setting name : String
-    setting support_email : String
+    setting support_email : Carbon::Address
   end
 end
 
 Application.configure do |settings|
   settings.name = "Lucky Jumpstart"
-  settings.support_email = "no-reply@lucky_jumpstart.com"
+  settings.support_email = Carbon::Address.new(settings.name, "no-reply@lucky_jumpstart.com")
 end
